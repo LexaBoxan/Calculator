1648b506d43b5dbad4ae8a4ae21e098fd87d72da
LexaBoxan <alesha.valeev@gmail.com>
2026-02-17 17:47:10 +0700

    Классс интерфейса

---
+++ interface.py
@@ -0,0 +1,4 @@
+class InterfaceCalculator:
+    def knipka():
+        pass
+    
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 def main():
     print(plus(6,7))
+    
 def plus(a, b):
     return a + b
 def multiplication(a, b):
